4aff0bf4054af088bd63366374196bbb6934a1ea
romulogomes <romulogomes13@hotmail.com>
2025-10-05 15:41:56 -0300

    get base url

--- config.js
+++ config.js
@@ -1,9 +1,20 @@
 (function() {
     'use strict';
+
+    function getBaseUrl() {
+        var hostname = window.location.hostname;
+        
+        if (hostname === 'localhost' || hostname === '127.0.0.1') {
+            return 'http://localhost:3000';
+        }
+        
+        // Otherwise, use production API
+        return 'https://ouvidoria-api-74b0526ccdf6.herokuapp.com';
+    }
     
     angular.module('app')
         .constant('API_CONFIG', {
-            BASE_URL: 'http://localhost:3000',  // Rails API URL
+            BASE_URL: getBaseUrl(),
             TIMEOUT: 10000,
             HEADERS: {
                 'Content-Type': 'application/json',
